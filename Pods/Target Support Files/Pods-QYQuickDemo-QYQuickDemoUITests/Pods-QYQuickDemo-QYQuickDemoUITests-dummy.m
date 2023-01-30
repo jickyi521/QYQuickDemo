2daf0d067890e3b9d053e9ffb73a9f4d2445655d
qianyi <qianyi@ydl.com>
2023-01-30 17:07:23 +0800

    update .gitignore

--- Pods/Target Support Files/Pods-QYQuickDemo-QYQuickDemoUITests/Pods-QYQuickDemo-QYQuickDemoUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_QYQuickDemo_QYQuickDemoUITests : NSObject
-@end
-@implementation PodsDummy_Pods_QYQuickDemo_QYQuickDemoUITests
-@end
--- Pods/Target Support Files/Pods-QYQuickDemoTests/Pods-QYQuickDemoTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_QYQuickDemoTests : NSObject
-@end
-@implementation PodsDummy_Pods_QYQuickDemoTests
-@end
