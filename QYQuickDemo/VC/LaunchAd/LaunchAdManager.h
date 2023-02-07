eda520aa8eb3aa564d41ce89f02689aecd7a1d1a
qianyi <jickyi521@gmail.com>
2023-02-07 13:50:21 +0800

    FEATURE: 新增开屏广告相关

---
+++ QYQuickDemo/VC/LaunchAd/LaunchAdManager.h
@@ -0,0 +1,16 @@
+//
+//  LaunchAdManager.h
+//  QYQuickDemo
+//
+//  Created by qianyi on 2023/2/6.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LaunchAdManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
